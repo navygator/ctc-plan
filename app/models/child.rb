596a41ac80a46bce9cf5798fbd5aced1fa8f694f
Alexey Nureev <navygator@myopera.com>
2015-01-18 19:52:45 +0400

    Fix scoping

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,3 @@
 class Child < Person
-  default_scope -> { where(child: true)}
+  default_scope -> { where(child: true) }
 end
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,3 +1,3 @@
 class Parent < Person
-  default_scope -> { where(child: false)}
+  default_scope -> { where(child: false) }
 end
